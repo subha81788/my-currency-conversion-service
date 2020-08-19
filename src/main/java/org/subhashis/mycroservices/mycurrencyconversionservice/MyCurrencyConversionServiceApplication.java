57bb501a11040296a8ace1fb425a26b6ab9b5694
subha81788 <subha81788@gmail.com>
2020-08-19 10:55:30 +0530

    updated pom

--- src/main/java/org/subhashis/mycroservices/mycurrencyconversionservice/MyCurrencyConversionServiceApplication.java
+++ src/main/java/org/subhashis/mycroservices/mycurrencyconversionservice/MyCurrencyConversionServiceApplication.java
@@ -7,9 +7,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
 @EnableFeignClients("org.subhashis.mycroservices.mycurrencyconversionservice")
 @EnableDiscoveryClient
+@SpringBootApplication
 public class MyCurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
